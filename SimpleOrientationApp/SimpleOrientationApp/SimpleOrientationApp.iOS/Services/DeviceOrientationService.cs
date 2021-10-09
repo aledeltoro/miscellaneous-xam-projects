f8db7fe5d594ad3696f1529e8d2f4eb812730bee
aledeltoro <alejandroencarnacion1@gmail.com>
2021-10-09 18:22:37 -0400

    Implement project which outputs device orientation

---
+++ SimpleOrientationApp/SimpleOrientationApp/SimpleOrientationApp.iOS/Services/DeviceOrientationService.cs
@@ -0,0 +1,25 @@
+﻿using Foundation;
+using SimpleOrientationApp.iOS.Services;
+using SimpleOrientationApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms.Internals;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DeviceOrientationService))]
+namespace SimpleOrientationApp.iOS.Services
+{
+    public class DeviceOrientationService : IDeviceOrientationService
+    {
+        public DeviceOrientation GetOrientation()
+        {
+            UIInterfaceOrientation orientation = UIApplication.SharedApplication.StatusBarOrientation;
+
+            bool isPortrait = orientation == UIInterfaceOrientation.Portrait || orientation == UIInterfaceOrientation.PortraitUpsideDown;
+
+            return isPortrait ? DeviceOrientation.Portrait : DeviceOrientation.Landscape;
+        }
+    }
+}
---
+++ SimpleOrientationApp/SimpleOrientationApp/SimpleOrientationApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using SimpleOrientationApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SimpleOrientationApp.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public string DeviceOrientation { get; set; }
+        public ICommand GetDeviceOrientationCommand { get; }
+
+        public MainViewModel()
+        {
+            GetDeviceOrientationCommand = new Command(GetOrientation);
+        }
+
+        private void GetOrientation()
+        {
+            var orientation = DependencyService.Get<IDeviceOrientationService>().GetOrientation();
+
+            DeviceOrientation = orientation.ToString();
+        }
+    }
+}
