947140edf0c07336d9ad8695bf25a233a6a0e8e4
aledeltoro <alejandroencarnacion1@gmail.com>
2021-10-09 23:31:12 -0400

    Implement project with two android specfic features

---
+++ AndroidSpecificFeatures/AndroidSpecificFeatures/AndroidSpecificFeatures/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndroidSpecificFeatures.Models
+{
+    public class Person
+    {
+        public Person(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+
+        public string Name { get; }
+        public string LastName { get; }
+    }
+}
